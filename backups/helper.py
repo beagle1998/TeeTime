db5bf227c58f8c05ba55a0b517ec09d1284e2c1e
Tyler Mun <33245978+beagle1998@users.noreply.github.com>
2025-08-10 15:27:04 -0700

    Update helper.py

--- backups/helper.py
+++ backups/helper.py
@@ -46,6 +46,7 @@ days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"] 
 book_list = []
 
 #insert password here
+email, password = os.environ['EMAIL'], os.environ['PASSWORD'] #cho credit
 
 operational = "Online"
 service = None 
@@ -286,21 +287,13 @@ async def on_message(message):
         operational = "offline"
     if msg.startswith('~start'):
         operational = "online"
-#api_key="E20N9WNT3FMJ"
 
 
+# print(os(dir_path).getenv("TOKEN"))
+# print(os.getenv("TOKEN"))
+# client.run(os.getenv("TOKEN"))
 
-
-
-
-
-
-
-print(os(dir_path).getenv("TOKEN"))
-print(os.getenv("TOKEN"))
-client.run(os.getenv("TOKEN"))
-
-
+client.run(os.getenvon("TOKEN"))
 
 
 
